59b16a73627e06c31097af3d288a5a3c8e1425a7
@vamsi753 <ltybjy666@gmail.com>
2023-11-08 15:53:59 +0530

    first commit

---
+++ Stack/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        static void runApp()
+        {
+            Stack stack = new Stack();
+
+            Console.WriteLine("Pushing elements onto the stack:");
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+
+            Console.WriteLine("Peek: " + stack.Peek());
+
+            Console.WriteLine("Popping elements from the stack:");
+            while (!stack.IsEmpty())
+            {
+                Console.WriteLine("Popped: " + stack.Pop());
+            }
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Stack
+{
+    private Node top;
+
+    public Stack()
+    {
+        top = null;
+    }
+
+    public void Push(int data)
+    {
+        Node newNode = new Node(data);
+        newNode.Next = top;
+        top = newNode;
+    }
+
+    public int Pop()
+    {
+        if (top == null)
+        {
+            throw new InvalidOperationException("The stack is empty.");
+        }
+
+        int data = top.Data;
+        top = top.Next;
+        return data;
+    }
+
+    public int Peek()
+    {
+        if (top == null)
+        {
+            throw new InvalidOperationException("The stack is empty.");
+        }
+
+        return top.Data;
+    }
+
+    public bool IsEmpty()
+    {
+        return top == null;
+    }
+}
